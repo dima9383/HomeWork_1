9d8fda33bb0170428389a7d5f5743247488248c5
555 <dima9383@gmail.com>
2021-09-08 15:24:45 +0300

    8

--- src/com/company/Task_8.java
+++ src/com/company/Task_8.java
@@ -6,14 +6,13 @@ public class Task_8 {
         int step = 0;
         int ind = 0;
         boolean forward = true;
-        boolean rubAdd = false;
 
         int array[] = {1, 2, 3, 4, 5, 3, 2, 1, 2, 6, 5, 4, 3, 3, 2, 1, 1, 3, 3, 3, 4, 2};
         int arrayRub[] = new int[array.length];
 
         for (int i = 0; i < array.length; i++) {
             arrayRub[i] = 1;
-            System.out.print(arrayRub[i] + " ");
+            System.out.print(array[i] + " ");
         }
 
         while (step < array.length) {
@@ -21,28 +20,36 @@ public class Task_8 {
             forward = step % 2 == 1;
 
             for (int i = 1; i < array.length; i++) {
-                rubAdd = false;
+
                 if (forward) {
                     ind = i;
                 } else {
                     ind = array.length - 1 - i;
                 }
 
-                if (forward && (arrayRub[ind] > arrayRub[ind - 1])) {
-                    arrayRub[ind] += 1;
-                    rubAdd = true;
-                }
+                if (forward) {
+                    if (array[ind] > array[ind - 1] && arrayRub[ind] <= arrayRub[ind - 1]) {
+                        arrayRub[ind] += 1;
+                    }
+                } else {
+                    if(array[ind] > array[ind + 1] && arrayRub[ind] <= arrayRub[ind + 1]){
+                        arrayRub[ind] += 1;
+                    }
 
-                if (!rubAdd) {
-                    break;
                 }
 
             }
+
+//            System.out.println();
+//            for (int i = 0; i < array.length; i++) {
+//                System.out.print(arrayRub[i] + " ");
+//            }
+
         }
 
         System.out.println();
         for (int i = 0; i < array.length; i++) {
-            System.out.print(arrayRub[i] + " ");
+            System.out.println(arrayRub[i] + " ");
         }
 
     }
